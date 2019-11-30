9365a51b1db40e5077f2e3ed2fe944ba6b7e8171
appalse <natalia.kutina13@gmail.com>
2019-12-01 00:01:30 +0300

    add Details component realization

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { ProductConsumer } from '../context';
+import { Link } from 'react-router-dom';
+import { ButtonContainer } from './Button';
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h3>hello from Details</h3>
-            </div>
+            <ProductConsumer>
+                {(value) => {
+                    const {id, type, img, info, price, title, inCart} = value.detailObject;
+                    return (
+                        <div className="container py-5">
+                            {/* title */}
+                            <div className="row">
+                                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                    <h1>{title}</h1>
+                                </div>
+                            </div>
+                            {/* end title */}
+
+                            {/* product info */}
+                            <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3">
+                                    <img src={img} className="img-fluid rounded-lg" alt="product" />
+                                </div>
+                                {/* product text */}
+                                <div className="col-10 mx-auto col-md-6 my-3 text-uppercase">
+                                    <h2>name: {title}</h2>
+                                    <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                                        object type : 
+                                        <span className="text-uppercase">{type}</span>
+                                    </h4>
+                                    <h4 className="text-blue">
+                                        <strong>
+                                            price : <span>$</span>
+                                            {price}
+                                        </strong>
+                                    </h4>
+                                    <p className="text-uppercase font-weight-bold mt-3 mb-0">
+                                        some info about object:
+                                    </p>
+                                    <p className="text-muted lead">
+                                        {info}
+                                    </p>
+                                    {/* buttons */}
+                                    <div>
+                                        <Link to='/'>
+                                            <ButtonContainer>
+                                                back to catalog
+                                            </ButtonContainer>
+                                        </Link>
+                                        <ButtonContainer disabled={inCart ? true : false}>
+                                            {inCart ? 'in cart' : 'add to cart'}
+                                        </ButtonContainer>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }}
+            </ProductConsumer>
         )
     }
 }
