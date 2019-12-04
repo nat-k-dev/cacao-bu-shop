2e29e0099e1d7cb84cfd9444cf27df12fc66d792
appalse <natalia.kutina13@gmail.com>
2019-12-04 18:22:16 +0300

    Updates

---
+++ precache-manifest.5cb4062c0a76a81be02df04340bf896a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8831b0af599729bdd26b214bd9ce85db",
+    "url": "/messier-objects/index.html"
+  },
+  {
+    "revision": "0620b28ab60a7b605f0f",
+    "url": "/messier-objects/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "bc1a4e345d9a537116b2",
+    "url": "/messier-objects/static/css/main.5428b6b5.chunk.css"
+  },
+  {
+    "revision": "0620b28ab60a7b605f0f",
+    "url": "/messier-objects/static/js/2.067eba0d.chunk.js"
+  },
+  {
+    "revision": "bc1a4e345d9a537116b2",
+    "url": "/messier-objects/static/js/main.e4f33492.chunk.js"
+  },
+  {
+    "revision": "80b32d056a1df881fd23",
+    "url": "/messier-objects/static/js/runtime-main.8eadcbe4.js"
+  },
+  {
+    "revision": "85d4ba53e5c75287e96d011dde83558f",
+    "url": "/messier-objects/static/media/logo.85d4ba53.svg"
+  }
+]);
