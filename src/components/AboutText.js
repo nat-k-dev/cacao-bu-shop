b20fb21f954ccc848cbbf7e3ff12063c4fbf830a
appalse <frontend-hh@yandex.ru>
2022-08-22 15:22:05 +0200

    add span for emodji

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -5,30 +5,30 @@ export default class AboutText extends Component {
         return (
             <React.Fragment>
                 <p className="mt-3 mb-3 text-center">
-                    Hello, I'm Olga! All my life I preferred to eat salty (for no reason), until I got pregnant.
-                    And then my taste preferences were turned upside down, so much so that every day I could not calmly walk past the chocolate counter and daily ate a whole bar 🍫
+                    Hello, I am Olga! All my life I preferred to eat salty (for no reason), until I got pregnant.
+                    And then my taste preferences were turned upside down, so much so that every day I could not calmly walk past the chocolate counter and daily ate a whole bar <span role="img" aria-label="Chocolate bar">🍫</span>
                 </p>
                 <p className="mt-3 mb-3 text-center">
                     After the birth of my son, the desire for chocolate became even stronger, this was also affected by lack of sleep, the body needed to draw energy from outside, and powerful hormone surges! And since I was breastfeeding, I began to think about the usefulness, or rather the danger of those sweets that I inexorably sweep away on my way!
                 </p>
                 <p className="mt-3 mb-3 text-center"> 
-                    So, not the fastest way, I came to the preparation of a healthy delicacy - artisan organic chocolate. And as it turned out, this is a whole vast world of bright tastes and colors, alluring magic of cooking, I will say even more - for me this is meditation and a way to replenish, as it is now in trend to say, my resource! Having tried it once, I can't stop not only eating chocolate, but also cooking it!
+                    So, not the fastest way, I came to the preparation of a healthy delicacy - artisan organic chocolate. And as it turned out, this is a whole vast world of bright tastes and colors, alluring magic of cooking, I will say even more - for me this is meditation and a way to replenish, as it is now in trend to say, my resource! Having tried it once, I cannot stop not only eating chocolate, but also cooking it!
                 </p>
                 <p className="mt-3 mb-3 text-center"> 
                     And now I can tell you for sure that healthy can be sooo tasty!
                 </p>
                 <p className="mt-3 mb-3 text-center"> 
-                    ❤️ LET YOUR LIFE BE DELICIOUS AND MEMORABLE ❤️
+                    <span role="img" aria-label="Red heart">❤️</span> LET YOUR LIFE BE DELICIOUS AND MEMORABLE <span role="img" aria-label="Red heart">❤️</span>
                 </p>
                 <p className="mt-3 mb-3 text-center">
-                ⏩ Home-made natural chocolate
+                <span role="img" aria-label="Arrows to the right">⏩</span> Home-made natural chocolate
                 </p>
                 <p className="mt-3 mb-3 text-center">
-                    🚫 Without lecithin
+                    <span role="img" aria-label="Stop sign">🚫</span> Without lecithin
                     <br></br>
-                    🚫 No refined sugar
+                    <span role="img" aria-label="Stop sign">🚫</span> No refined sugar
                     <br></br>
-                    🚫 Lactose free
+                    <span role="img" aria-label="Stop sign">🚫</span> Lactose free
                 </p>
 
             </React.Fragment>
