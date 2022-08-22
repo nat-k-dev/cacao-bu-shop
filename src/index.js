12d3f41d73f88204c86574eb8b7b94ea435ea868
appalse <frontend-hh@yandex.ru>
2022-08-22 16:59:57 +0200

    replace ReactDOM.render by createRoot

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ProductProvider } from './context';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <ProductProvider>
-        <Router>
-            <App />
-        </Router>
-    </ProductProvider>,
-    document.getElementById('root'));
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<ProductProvider>
+                <Router>
+                    <App />
+                </Router>
+            </ProductProvider>);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
