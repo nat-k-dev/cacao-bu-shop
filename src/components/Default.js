f6b102e1261b467c5dc95647676461e9808cbc64
appalse <frontend-hh@yandex.ru>
2022-08-22 15:19:44 +0200

    add PropTypes for Page Not Found

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
 export default class Default extends Component {
+    static propTypes = { 
+        location: PropTypes.any
+    }
     render() {
         console.log(this.props);
         return (
@@ -10,13 +14,13 @@ export default class Default extends Component {
                         <h1 className="display-3">404</h1>
                         <h1>error</h1>
                         <h2>page not found</h2>
-                        <h3>the requested URL:" 
+                        <h3>the requested URL:&quot 
                             <span 
                                 className="text-danger"
                             >
                                 {this.props.location.pathname}
                             </span>
-                            " was not found</h3>
+                            &quot was not found</h3>
                     </div>
                 </div>
             </div>
