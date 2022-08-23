d6e67023350c388587823bd28e5bdfa916c5be28
appalse <frontend-hh@yandex.ru>
2022-08-24 00:18:50 +0200

    add styled button for sending order from cart

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function CartTotals({value}) {
     const { cartSubTotal, cartTax, cartTotal, clearCart } = value; 
@@ -9,15 +10,26 @@ export default function CartTotals({value}) {
             <div className="container">
                 <div className="row">
                     <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-uppercase text-right">
-                        <Link to='/'>
-                            <button 
-                                className="btn btn-outline-danger text-uppercase mb-3 px-5" 
-                                type="button"
-                                onClick={() => clearCart()}
-                            >
-                                clear cart
-                            </button>
-                        </Link>
+                        <CartButtonStyle>
+                            <Link to='/'>
+                                <button 
+                                    className="btn btn-outline-light text-uppercase mb-3 px-5 cart-button" 
+                                    type="button"
+                                    onClick={() => clearCart()}
+                                >
+                                    send order
+                                </button>
+                            </Link>
+                            <Link to='/'>
+                                <button 
+                                    className="btn btn-outline-danger text-uppercase mb-3 px-5 cart-button" 
+                                    type="button"
+                                    onClick={() => clearCart()}
+                                >
+                                    clear cart
+                                </button>
+                            </Link>
+                        </CartButtonStyle>
                         <h5>
                             <span className="text-title">
                                 subtotal : 
@@ -46,3 +58,17 @@ export default function CartTotals({value}) {
 CartTotals.propTypes = {
     value: PropTypes.any, 
 }
+
+const CartButtonStyle = styled.div`
+    .cart-button {
+        display: block;
+        margin-left: auto;
+        margin-right: 0;
+        width: 200px;
+    }
+    @media only screen and (max-width: 300px) {
+        .cart-button {
+            width: 100%;
+        }
+    }
+`
