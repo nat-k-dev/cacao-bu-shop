8b808412f32360b22664ecb2303ed8752788e0ea
appalse <frontend-hh@yandex.ru>
2022-08-24 00:00:01 +0200

    add ;

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
-import Modal from './components/Modal'
+import Modal from './components/Modal';
 
 class App extends Component {
   render() {
